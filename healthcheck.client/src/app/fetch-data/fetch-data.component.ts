e74786e4ecb7a5811448bcbcd5788412aaadc8d1
luis-eduardo <luis-eduardo@users.noreply.github.com>
2025-10-07 10:04:19 -0400

    Refactor app structure and add routing support.

---
+++ healthcheck.client/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-fetch-data',
+  standalone: false,
+  templateUrl: './fetch-data.component.html',
+  styleUrl: './fetch-data.component.css'
+})
+export class FetchDataComponent {
+  public forecasts?: WeatherForecast[] = [];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+}
