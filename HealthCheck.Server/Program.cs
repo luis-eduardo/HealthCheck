c94aa1493920b3aae1d2e107013ab5901c607403
luis-eduardo <luis-eduardo@users.noreply.github.com>
2025-10-07 13:15:36 -0400

    Display health check in angular app

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddHealthChecks()
-    .AddCheck("ICMP_01", new ICMPHealthCheck("www.rydael.com", 100))
+    .AddCheck("ICMP_01", new ICMPHealthCheck("www.bing.com", 100))
     .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
     .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
 builder.Services.AddControllers();
---
+++ healthcheck.client/src/app/health-check/health-check.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+interface Result {
+  checks: Check[]
+  totalStatus: string;
+  totalResponseTime: number;
+}
+
+interface Check {
+  name: string;
+  status: string;
+  responseTime: number;
+  description: string;
+}
+
+@Component({
+  selector: 'app-health-check',
+  standalone: false,
+  templateUrl: './health-check.component.html',
+  styleUrl: './health-check.component.css'
+})
+export class HealthCheckComponent implements OnInit {
+  public result?: Result;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Result>(environment.baseUrl + 'api/health').subscribe(
+      result => {
+        this.result = result;
+      },
+      error => console.error(error)
+    );
+  }
+}
